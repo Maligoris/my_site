60b226ddeee6a685deae8fd9cf30f0374d755403
Sergey <iqgenius@yandex.ru>
2021-04-27 13:31:05 +0300

    heroku push

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['S_KEY'] # Ключ в переменной окруже
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
